d90c271101f591826ff6a26bd56d72693f26948c
Danny-Nicolas GRANGEOT <danny-nicolas.grangeot@gmail.com>
2024-03-20 22:52:25 +0100

    🛂 Added Auth

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
-
+import FeedView from '@/views/FeedView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,9 +14,30 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: LoginView,
+    },
+    {
+      path: '/feed',
+      name: 'feed',
+      component: FeedView,
+      meta: {
+        requiresAuth: true
+      }
     }
     
   ]
 })
 
+router.beforeEach((to, from, next) => {
+
+  let token = document.cookie.split('; ').find(row => row.startsWith('token='))
+  if (token) {
+    token = token.split('=')[1]
+  }
+  if (to.meta.requiresAuth && !token) {
+    next('/login')
+  }
+  else {
+    next()
+  }
+})
 export default router
---
+++ src/views/FeedView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        le feed ici
+    </div>
+</template>
+
