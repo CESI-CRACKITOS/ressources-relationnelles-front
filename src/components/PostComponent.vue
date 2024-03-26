bdc57e1f64c1456608c10370cb25a16ed92b192c
Paul ETIGNARD <Paul.etignard@gmal.com>
2024-03-26 15:14:48 +0100

    ✨ Refactor PostComponent.vue and ProfileView.vue***
    ***Update PostComponent.vue and ProfileView.vue***
    
    ***Fix formatting and import issues in PostComponent.vue and ProfileView.vue

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,74 +1,66 @@
 <template>
-  <div
-    class="flex bg-white p-3 border-b border-gray-200 gap-2"
-    @click="navigateToResourceDetails(resource.id)"
-  >
-    <div class="w-12">
-      <img src="./1681453032243.jpg" class="rounded-lg" alt="" />
-    </div>
-    <div class="flex flex-col w-full gap-2">
-      <div class="flex flex-row justify-between w-full">
-        <div class="flex flex-row gap-2 items-center">
-          <h3>{{ resource.user?.firstname + ' ' + resource.user?.lastname }}</h3>
-          <span class="text-xs h-fit">7s</span>
-        </div>
-        <i class="fas fa-ellipsis"></i>
-      </div>
-      <div>
-        <p>
-          {{ resource.title }}
-        </p>
-        <p v-for="content in resource.contents" :key="content.id">
-          {{ content.text }}
-        </p>
-      </div>
+	<div class="flex bg-white p-3 border-b border-gray-200 gap-2" @click="navigateToResourceDetails(resource.id)">
+		<div class="w-12">
+			<img src="./1681453032243.jpg" class="rounded-lg" alt="" />
+		</div>
+		<div class="flex flex-col w-full gap-2">
+			<div class="flex flex-row justify-between w-full">
+				<div class="flex flex-row gap-2 items-center">
+					<h3>{{ resource.user?.firstname + ' ' + resource.user?.lastname }}</h3>
+					<span class="text-xs h-fit">{{ tempsDepuisPost }}</span>
+				</div>
+				<i class="fas fa-ellipsis"></i>
+			</div>
+			<div>
+				<p>
+					{{ resource.title }}
+				</p>
+				<p v-for="content in resource.contents" :key="content.id">
+					{{ content.text }}
+				</p>
+			</div>
 
-      <div class="w-full flex flex-wrap">
-        <div class="w-full h-56 flex items-center justify-center border rounded-xl">
-          le gros jeu de débille
-        </div>
-      </div>
-      <div>
-        <div class="flex flex-row items-center justify-start gap-2">
-          <IconButtonComponentVue
-            icon="far fa-heart"
-            action="like"
-            :context-id="resource.id"
-            active="true"
-          />
-          <IconButtonComponentVue
-            icon="far fa-comment"
-            action="comment"
-            :context-id="resource.id"
-            active="true"
-          />
-          <IconButtonComponentVue
-            icon="fas fa-retweet"
-            action="retweet"
-            :context-id="resource.id"
-            active="true"
-          />
-        </div>
-      </div>
-    </div>
-  </div>
+			<div class="w-full flex flex-wrap">
+				<div class="w-full h-56 flex items-center justify-center border rounded-xl">
+					le gros jeu de débille
+				</div>
+			</div>
+			<div>
+				<div class="flex flex-row items-center justify-start gap-2">
+					<IconButtonComponentVue icon="far fa-heart" action="like" :context-id="resource.id" active="true" />
+					<IconButtonComponentVue icon="far fa-comment" action="comment" :context-id="resource.id"
+						active="true" />
+					<IconButtonComponentVue icon="fas fa-retweet" action="retweet" :context-id="resource.id"
+						active="true" />
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
 import ResourceEntity from '@/composable/Entities/Resource'
 import IconButtonComponentVue from './shared/buttons/IconButtonComponent.vue'
 import router from '@/router'
+import { calculateDates } from "@/composable/Utils/DateUtils"
+import { onMounted, ref } from 'vue';
 
 const props = defineProps({
-  resource: {
-    type: ResourceEntity,
-    required: true
-  }
+	resource: {
+		type: ResourceEntity,
+		required: true
+	}
+})
+let tempsDepuisPost = ref("")
+onMounted(async() => {
+	tempsDepuisPost.value = await calculateDates(props.resource.created_at)
 })
 
-console.log(props.resource.user)
+
 
 function navigateToResourceDetails(id: number) {
-  router.push({ name: 'resource', params: { id: id } })
+	router.push({ name: 'resource', params: { id: id } })
 }
+
+
 </script>
--- src/composable/Utils/DateUtils.ts
+++ src/composable/Utils/DateUtils.ts
@@ -1,7 +1,44 @@
-function formatDate(dateString : string) {
-    const date = new Date(dateString);
-        // Then specify how you want your dates to be formatted
-    return new Intl.DateTimeFormat('fr-FR', {dateStyle: 'long'}).format(date);
+export function formatDate(dateString: string) {
+  const date = new Date(dateString)
+  // Then specify how you want your dates to be formatted
+  return new Intl.DateTimeFormat('fr-FR', { dateStyle: 'long' }).format(date)
 }
 
-export default formatDate
+export async function calculateDates(date: Date) {
+  const startDate = new Date(date)
+
+  const endDate = new Date()
+
+  let secondsBetweenDates = endDate.getTime() - startDate.getTime()
+  secondsBetweenDates = secondsBetweenDates / 1000 //FIXME
+
+  const seconds = secondsBetweenDates % 60
+  const minutes = Math.floor(secondsBetweenDates / 60) % 60
+  const hours = Math.floor(secondsBetweenDates / 3600) % 24
+  const days = Math.floor(secondsBetweenDates / 86400) % 30.5
+  const months = Math.floor(secondsBetweenDates / 2629746) % 12
+  const years = Math.floor(secondsBetweenDates / 31556952)
+
+  const timeUnits = [
+    formatTime(years, 'an'),
+    formatTime(months, 'mois'),
+    formatTime(days, 'jour'),
+    formatTime(hours, 'heure'),
+    formatTime(minutes, 'minute'),
+    formatTime(seconds, 'seconde')
+  ]
+
+  let tempsDepuisCreation = timeUnits
+    .filter((unit) => unit !== '')
+    .slice(0, 2)
+    .join('')
+  tempsDepuisCreation = tempsDepuisCreation.endsWith('')
+    ? tempsDepuisCreation.slice(0, -2)
+    : tempsDepuisCreation
+
+  return tempsDepuisCreation
+}
+
+function formatTime(value, unit) {
+  return value > 0 ? `${parseFloat(value).toFixed(0)} ${unit}${value > 1 ? 's' : ''}, ` : ''
+}
