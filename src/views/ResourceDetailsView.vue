bb84655daef86186d9a94ab3f0f827041a96b3b8
Paul ETIGNARD <Paul.etignard@gmal.com>
2024-03-26 15:35:47 +0100

    :pencil2: Refactor ResourceDetailsView.vue

--- src/views/ResourceDetailsView.vue
+++ src/views/ResourceDetailsView.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <PostComponent :key="resource.id" :resource="resource" />
-    <div v-for="comment in resource.comments" :key="comment.id" class="flex bg-white p-3 border-b border-gray-200 gap-2">
+    <div
+      v-for="comment in resource.comments"
+      :key="comment.id"
+      class="flex bg-white p-3 border-b border-gray-200 gap-2"
+    >
       <CommentComponent :comment="comment" />
     </div>
   </div>
@@ -15,15 +19,11 @@ import { onMounted, ref } from 'vue'
 import router from '@/router'
 import CommentComponent from '@/components/CommentComponent.vue'
 
-
 let resource = ref<ResourceEntity[]>([])
 
 let idResource = router.currentRoute.value.params.id
 
 onMounted(async () => {
-
   resource.value = await getResourceById(idResource)
-
-  console.log(resource.value)
 })
 </script>
