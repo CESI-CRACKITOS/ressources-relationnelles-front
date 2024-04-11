fd1fcaa5dc7d7e03e15b05622f7a33d4832e9141
Danny-Nicolas GRANGEOT <danny-nicolas.grangeot@gmail.com>
2024-04-11 16:33:25 +0200

    :sparkles: Add Vite PWA plugin to package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,33 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Vue 3 Vite PWA',
+        short_name: 'V3VPWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
