c42d39cc8cd71565c914819ca7dd2aadf208ea04
Danny-Nicolas GRANGEOT <danny-nicolas.grangeot@gmail.com>
2024-04-17 19:31:33 +0200

    :sparkles: Refactor App.vue, CategorieTrendComponent.vue, and CategorieResourcesListeView.vue

--- src/views/CategorieResourcesListeView.vue
+++ src/views/CategorieResourcesListeView.vue
@@ -1,5 +1,20 @@
 <template>
-  <p>ouiyhvouyjhcvo</p>
+  <div class="flex h-screen overflow-scroll flex-col">
+    <PostComponent v-for="resource in resources" :key="resource.id" :resource="resource" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type ResourceEntity from '@/composable/Entities/Resource'
+import { getResourcesFromCategoryId } from '@/composable/Utils/ResourcesUtils'
+import router from '@/router'
+import PostComponent from '@/components/PostComponent.vue'
+import { onMounted, ref } from 'vue'
+
+let resources = ref<ResourceEntity[]>([])
+
+onMounted(async () => {
+  resources.value = await getResourcesFromCategoryId(router.currentRoute.value.params.id.toString())
+  console.log(resources)
+})
+</script>
