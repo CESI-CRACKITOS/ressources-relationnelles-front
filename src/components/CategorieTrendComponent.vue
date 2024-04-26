43bdeb1717da28d7ff28f7dff8e71345b5e866a0
THABUSSOT Téo <teo.thabussot.2b@gmail.com>
2024-04-26 23:46:39 +0200

    Fixed for real

--- src/components/CategorieTrendComponent.vue
+++ src/components/CategorieTrendComponent.vue
@@ -1,11 +1,12 @@
 <template>
-  <a :href="`/category/${id}`"
+  <RouterLink
+    :to="{ name: 'categoryResourcesList', params: { id: id } }"
     class="flex flex-col w-full hover:bg-gray-300 px-4 py-1"
   >
     <p class="text-sm text-gray-500">{{ TrendRanking }} - Tendance</p>
     <p class="font-semibold">{{ name }}</p>
     <p v-if="NumberOfPost" class="text-sm text-gray-500">{{ NumberOfPost }} ressource(s)</p>
-  </a>
+  </RouterLink>
 </template>
 
 <script setup lang="ts">
