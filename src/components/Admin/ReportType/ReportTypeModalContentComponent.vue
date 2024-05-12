1afde850607fb3a20400a86f5e32a79e15e88e9e
Danny-Nicolas GRANGEOT <danny-nicolas.grangeot@gmail.com>
2024-05-12 18:56:07 +0200

    refactor: Update label for report type in ReportTypeModalContentComponent.vue

--- src/components/Admin/ReportType/ReportTypeModalContentComponent.vue
+++ src/components/Admin/ReportType/ReportTypeModalContentComponent.vue
@@ -1,11 +1,16 @@
 <template>
-
-  <div class="flex flex-col bg-white max-h-[700px] max-w-[900px] overflow-y-auto rounded-md p-5 gap-5" @click.stop>
-    <label for="">Type de relation</label>
-    <input type="text" class="bg-gouv-gray-300 border-b-2 border-black w-full p-3 rounded-t-md" v-model="data.reportType.name">
+  <div
+    class="flex flex-col bg-white max-h-[700px] max-w-[900px] overflow-y-auto rounded-md p-5 gap-5"
+    @click.stop
+  >
+    <label for="">Type de signalment</label>
+    <input
+      type="text"
+      class="bg-gouv-gray-300 border-b-2 border-black w-full p-3 rounded-t-md"
+      v-model="data.reportType.name"
+    />
     <button-component @click="send()">Envoyer</button-component>
   </div>
-
 </template>
 <script setup>
 import { defineProps } from 'vue'
@@ -17,7 +22,7 @@ const props = defineProps({
 })
 
 const data = {
-  reportType: props.reportType ? props.reportType : {},
+  reportType: props.reportType ? props.reportType : {}
 }
 
 const send = async () => {
@@ -28,5 +33,4 @@ const send = async () => {
   }
   window.location.reload()
 }
-
 </script>
